5e7e324a75d0589a1828e32535b35e499fa75d92
Joseph Yougn <jryoun23@asu.edu>
2021-05-09 01:02:35 -0700

    going to bed

--- supertrendBot.py
+++ supertrendBot.py
@@ -1,4 +1,6 @@
 #the goal of this python bot is to analyze market data using the ccxt python package to retrieve data as well as possibly in the near future make buy and sell requests
+#this bot in particular is attempting to use a SUPERTREND trading strategy.
+#I plan to learn more about this strategy as I read into it.
 
 import config
 import ccxt
@@ -19,6 +21,21 @@ df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
 print(df)
 
 # here we are going to be computing an upper and lower band with the formulas as follows
+# It is going to be split up into two different sections 
 
-#upperband = 
-#lowerband = 
+#---------BASIC------------
+
+#basicUpperband = (High+Low)/2 + Multiplier*ATR
+#basicLowerband = (High+Low)/2 + Multiplier*ATR
+
+#---------FINAL------------
+
+# FinalUpperBand = if((Current.basicUpperband < Previous.FinalUpperband) && (Previous.close < Previous.FinalLowerBand))
+#                   {Current.BasicLowerband}
+#                  else
+#                   {PreviousFinalLowerband}
+
+# FinalUpperBand = if((Current.basicUpperband < Previous.FinalUpperband) && (Previous.close < Previous.FinalLowerBand))
+#                   {Current.BasicLowerband}
+#                  else
+#                   {PreviousFinalLowerband}
